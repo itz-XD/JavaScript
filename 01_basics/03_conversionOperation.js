ea33bc7ed8fcab6c50c0a2ee8e851d3388b0bda6
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-04-14 14:04:38 +0530

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+score = "33abc"
+score = null
+score = undefined
+score = true
+score = false
+score = "XD"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// "33" => 33
+// "33abc" => NaN (not an number)
+// true => 1; false => 0
+
+let isLoggedIn = `XD`
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "XD" => true
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
