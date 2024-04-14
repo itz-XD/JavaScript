c9a9fead830bac874dd2eba28aacfa0f9834df07
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-04-14 18:01:49 +0530

    Comparision Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,4 +29,36 @@ let valueInNumber = Number(score)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+
+
+
+/************************ Operations ************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " XD"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log((3 + 4) * 5 % 3)
