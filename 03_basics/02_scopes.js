f71ed3860da4f00baa146f53100f86cc999625c1
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-04-22 13:06:59 +0530

    Nested Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,9 +6,56 @@ if (true) {
     const b = 20
     var c = 30
     c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// ``````````````````````      ````````````````````
+
+function one() {
+    const username = "_XD"
+
+    function two() {
+        const website = "YouTube"
+        console.log("username: ", username);
+    }
+    // console.log("Website: ", website);
+
+    two()
+}
+
+// one() 
+
+
+if (true) {
+    const username = "+_XD"
+    if (username === "+_XD") {
+        const website = " YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// **********************   interesting   **********************
+
+// console.log(addone(5))  // <= all good :)
+
+function addone(num) {
+    return num + 1
+}
+// // addone(5)    // <= all set :)
+
+// `````````````````````````````````````````````````````````````````
+
+// addTwo(5)    // <= cause Error ;)
+const addTwo = function(num) {
+    return num + 2
+}
+// addTwo(5)    //  <= no Error :)
