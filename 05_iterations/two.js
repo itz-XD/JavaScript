49bb1f4e330dddd63f6644783a789cff5866bca9
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-05-05 15:27:48 +0530

    while & do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while
+
+/*
+let index = 0;
+while (index <= 10) {
+    console.log(`Value of index is: ${index}`);
+    index += 2;
+}
+*/
+
+console.log("\n");
+
+/*
+let myArray = ['flash', 'batman', 'superman']
+let i = 0;
+while (i < myArray.length) {
+    console.log(`Value is: ${myArray[i]}`);
+    i += 1;
+}
+*/
+
+console.log("\n");
+
+let n=6;
+do {
+    console.log(`${n}: Hello World!`);
+    n = n + 1;
+} while (n <= 5);
