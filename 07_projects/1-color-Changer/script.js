044b8142066352a2ac5e2086073c3a436ee843bb
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-05-16 19:24:09 +0530

    project 1 solution code

---
+++ 07_projects/1-color-Changer/script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body')
+// console.log(buttons);
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach((button) => {
+    // console.log(button);
+    button.addEventListener('click', (e) => {
+        // console.log(e);
+        // console.log(e.target);
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
