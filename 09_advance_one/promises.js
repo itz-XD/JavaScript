3c17869d9dbe8b733c32b5648c31da38c422f36d
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-06-01 12:23:22 +0530

    ApiRequest

--- 09_advance_one/promises.js
+++
@@ -1,4 +0,0 @@
-/* To make it simple, an instance is like a unique copy of a blueprint.
-         Imagine you have a blueprint for a house. The blueprint itself is not a house, but using it, you can build as many houses as you want.
-                 Each house you build using the blueprint is an instance of that blueprint. */
-
